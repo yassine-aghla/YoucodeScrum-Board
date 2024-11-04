de30bc75334f8e033d80257dc705e8a0cc2e36af
yassine <yassineaghla23@gmail.com>
2024-11-04 14:22:11 +0100

    comment taskItem

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -28,35 +28,35 @@ document.getElementById("form-task").addEventListener("submit", function(event) 
 
 
 
-  taskItem.classList.add("list-group-item", "task-item")
-  taskItem.innerHTML = `
-   <button class="task">
-                  <div class="">
-                    <i class=""></i>
-                  </div>
-                  <div class="">
-                    <div class="">
-                    ${title}
-                    </div>
-                    <div class="">
-                      <div class=""> created in  ${date}</div>
-                      <div
-                        class=""
-                        title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly."
-                      >
-                      ${description}
-                      </div>
-                    </div>
-                    <div class="">
-                      <span class="btn btn-primary">${type}</span>
-                      <span class="btn btn-white">${priority}</span>
-                    </div>
-                    <button class="btn btn-danger btn-sm delete-task" id="task-delete-btn" onclick="DeleteTaskEvent(this)" >Supprimer</button>
-                    <button type="submit" name="update" class="btn btn-warning task-action-btn" id="task-update-btn" >Update</button>
-                  </div>
+  // taskItem.classList.add("list-group-item", "task-item")
+  // taskItem.innerHTML = `
+  //  <button class="task">
+  //                 <div class="">
+  //                   <i class=""></i>
+  //                 </div>
+  //                 <div class="">
+  //                   <div class="">
+  //                   ${title}
+  //                   </div>
+  //                   <div class="">
+  //                     <div class=""> created in  ${date}</div>
+  //                     <div
+  //                       class=""
+  //                       title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly."
+  //                     >
+  //                     ${description}
+  //                     </div>
+  //                   </div>
+  //                   <div class="">
+  //                     <span class="btn btn-primary">${type}</span>
+  //                     <span class="btn btn-white">${priority}</span>
+  //                   </div>
+  //                   <button class="btn btn-danger btn-sm delete-task" id="task-delete-btn" onclick="DeleteTaskEvent(this)" >Supprimer</button>
+  //                   <button type="submit" name="update" class="btn btn-warning task-action-btn" id="task-update-btn" >Update</button>
+  //                 </div>
                  
-                </button>
-  `;
+  //               </button>
+  // `;
 
   if (status === "To Do") {
     document.getElementById("to-do-tasks").appendChild(taskItem);
