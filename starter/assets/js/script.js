a8272c15a185bb65e684f61692e6b2abdb2a0666
yassine <yassineaghla23@gmail.com>
2024-11-04 20:54:53 +0100

    finir les fonctions de modifier et supprimer

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -8,17 +8,10 @@ function displaymodal() {
   }
  
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
- 
   const toDoTasksElement = document.getElementById('to-do-tasks');
   const inProgressTasksElement = document.getElementById('in-progress-tasks');
   const doneTasksElement = document.getElementById('done-tasks');
-  let title = document.getElementById("task-title")
-  let feature = document.getElementById('task-type-feature')
-  let type= document.getElementById('task-type-bug')
-  let date = document.getElementById("task-date")
-  let description = document.getElementById("task-description")
-  let donecount = document.getElementById("done-tasks-count");
+
  
   function loadTasks() {
     toDoTasksElement.innerHTML = '';
@@ -30,7 +23,33 @@ function displaymodal() {
     });
   }
   
- 
+  function saveTask() {
+    const title = document.getElementById('task-title').value;
+    const description = document.getElementById('task-description').value;
+    const priority = document.getElementById('task-priority').value;
+    const status = document.getElementById('task-status').value;
+    const check = document.getElementById('task-id').value;
+    const taskType = document.querySelector('input[name="task-type"]:checked').value;
+    const date = document.getElementById("task-date").value;
+    
+    
+    const task = { title, description,taskType, priority, status , type, date };
+  
+    if (check === ''){ 
+      // ajouter task
+      tasks.push(task); 
+    } else {
+      // modiifer la tache deja exite
+      tasks[check] = task;  
+    }
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    loadTasks();
+    hideenmodal();
+    document.getElementById('form-task').reset();
+  }
+
+
   function displayTask(task, index) {
     const taskElement = document.createElement('div');
     taskElement.classList.add('list-group-item');
@@ -39,10 +58,10 @@ function displaymodal() {
       <p>${task.description}</p>
     <p> crated in ${task.date}</p>
     <div >
-    <button class="btntype" > ${task.type}</button>
+    <button class="btntype" > ${task.taskType}</button>
       <button class="btnpriority"> ${task.priority}</button>
       </div>
-      <div class=update-delete>
+      <div class="update-delete">
         <button onclick="editTask(${index})" class="btn btn-warning btn-sm">Update</button>
         <button onclick="deleteTask(${index})" class="btn btn-danger btn-sm">Delete</button>
       </div>
@@ -50,56 +69,52 @@ function displaymodal() {
   
     if (task.status === 'To Do') {
       toDoTasksElement.appendChild(taskElement);
-      let numb = document.getElementById("to-do-tasks-count").childElementCount;
-    
+      let numb1 = document.getElementById("to-do-tasks-count");
+    numb1.textContent=toDoTasksElement.children.length
     } else if (task.status === 'In Progress') {
       inProgressTasksElement.appendChild(taskElement);
-      let numb = document.getElementById("in-progress-tasks-count").childElementCount;
-    
+      let numb2 = document.getElementById("in-progress-tasks-count");
+      numb2.textContent=inProgressTasksElement.children.length
     } else if (task.status === 'Done') {
       doneTasksElement.appendChild(taskElement);
-      let numb = document.getElementById("done-tasks-count").childElementCount;
-      
+      let numb3 = document.getElementById("done-tasks-count");
+      numb3.textContent=doneTasksElement.children.length
       
     }
   }
   
   
-  function saveTask() {
-    const title = document.getElementById('task-title').value;
-    const description = document.getElementById('task-description').value;
-    const priority = document.getElementById('task-priority').value;
-    const status = document.getElementById('task-status').value;
-    const index = document.getElementById('task-id').value;
-    const type= document.getElementById('task-type-bug').value;
-    const date = document.getElementById("task-date").value;
-    
-    const task = { title, description, priority, status , type, date };
+
   
-    if (index === '') {
-      tasks.push(task);
-    } else {
-      tasks[index] = task;
-    }
+ 
+  function editTask(index) {
+    displaymodal();
+  
+    const task = tasks[index];
     
+    document.getElementById('task-title').value = task.title;
+    document.getElementById('task-description').value = task.description;
+    document.getElementById('task-priority').value = task.priority;
+    document.getElementById('task-status').value = task.status;
+    document.getElementById('task-id').value = index;
+    document.querySelector('input[name="task-type"]:checked').value = task.taskType;
+    document.getElementById("task-date").value = task.date;
+  }
+  
+  
+  function deleteTask(index) {
+    tasks.splice(index, 1);
     localStorage.setItem('tasks', JSON.stringify(tasks));
     loadTasks();
-    hideModal();
-  } 
-  function showModal() {
-    document.getElementById('modal-dialog').style.display = 'block';
   }
   
-  function hideModal() {
-    document.getElementById('modal-dialog').style.display = 'none';
-    document.getElementById('form-task').reset();
-    document.getElementById('task-id').value = '';
-  }
   
 
   document.getElementById('task-save-btn').addEventListener('click', (e) => {
     e.preventDefault();
     saveTask();
+    
+    document.getElementById('task-id').value = '';
   });
   
  
